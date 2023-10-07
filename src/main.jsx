9cbc7be302abbf63d276a3975ff8a4e2e1b13d47
Oleksiy <oleksiy30k30@gmail.com>
2023-10-07 23:52:05 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './css/global/normalize.css'
+import './css/global/App.css';
+import './css/global/normalize.css';
+import './css/MainScreen/efects.css';
+import './css/MainScreen/page.css';
+import './css/global/typography.css';
+import './css/global/colors.css';
+import './css/homepage/background.css';
+import './css/homepage/header.css';
+import './css/homepage/sections.css';
+import './css/homepage/efects.css';
+import './css/homepage/lists.css';
+import './css/homepage/footer.css';
+import './css/mobile/mobile.css';
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
